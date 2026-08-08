92033a8984b9bc6d515f674b096f56d76dc0664e
Patrick Girardi <patrickgirardi@proton.me>
2026-08-08 15:05:07 -0300

    feat: update tech stack, add courses and interests sections, and replace Design Works card with Projects card

--- src/site-config.ts
+++ src/site-config.ts
@@ -1,23 +1,23 @@
 export const SITE = {
-  "site": {
-    "url": "https://patrickgirardi.dev.br"
+  site: {
+    url: "https://patrickgirardi.dev.br",
   },
-  "author": {
-    "firstName": "Patrick",
-    "lastName": "Girardi",
-    "fullName": "Patrick Girardi",
-    "shortName": "Patrick",
-    "jobTitle": "FullStack Web Developer",
-    "twitterHandle": "null"
+  author: {
+    firstName: "Patrick",
+    lastName: "Girardi",
+    fullName: "Patrick Girardi",
+    shortName: "Patrick",
+    jobTitle: "FullStack Web Developer",
   },
-  "location": {
-    "countryName": "Brazil",
-    "countryCode": "BR",
-    "timezone": "America/Sao_Paulo"
+  location: {
+    countryName: "Brazil",
+    countryCode: "BR",
+    timezone: "America/Sao_Paulo",
   },
-  "links": {
-    "github": "https://github.com/trickgirardi",
-    "linkedin": "https://www.linkedin.com/in/patrickgirardi/",
-    "email": "patrickgirardi@proton.me"
-  }
+  links: {
+    github: "https://github.com/trickgirardi",
+    linkedin: "https://www.linkedin.com/in/patrickgirardi/",
+    email:
+      "mailto:patrickgirardi@proton.me?subject=Contato pelo portfólio&body=Olá, Patrick!",
+  },
 };
